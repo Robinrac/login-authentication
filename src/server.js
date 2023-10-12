717bb4283dd872d1078d181433cdf6f9c05cb4bb
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-10-12 20:16:58 +0200

    (#24) Successful MongoDB database connection

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
+require("dotenv").config();
+
 const express = require('express');
 const cors = require('cors');
-
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
 const dataRoutes = require('./routes/data_routes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/data', dataRoutes);
+
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(process.env.DB_URL);
+      console.log("------------You have successfully connected MONGO DB------------");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+}
+
+connectToDatabase();
 
 module.exports = app;
