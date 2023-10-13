388b09b8f22d69da58a19523a5b533d29f757e38
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-10-14 01:05:03 +0200

    (#49) Bcrypt security added before saving user model

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Bcrypt = require("bcrypt")
 
 const userSchema = mongoose.Schema({
   emailAddress: {
@@ -18,5 +19,19 @@ const userSchema = mongoose.Schema({
     default: Date.now,
   },
 });
+
+//hash the passwords
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    const salt = await Bcrypt.genSalt(10);
+    this.password = await Bcrypt.hash(this.password, salt);
+    next();
+  } catch {
+    return next(error)
+  }
+})
 
 module.exports = mongoose.model("user", userSchema);
