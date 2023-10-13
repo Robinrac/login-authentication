eac2fb9575e2785080bcb5ebdb5df36b8a6af29c
Robin Raczkiewicz <robin.raczkiewicz@gmail.com>
2023-10-13 14:00:51 +0200

    (#29) Removed roll functionality + comments

--- src/routes/data_routes.js
+++ src/routes/data_routes.js
@@ -7,20 +7,17 @@ router.get('/', (req, res) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  //Om token inte finns, skicka status 402 "access denied"
   if (!token) return res.status(401).send('Access Denied');
 
-    //Om token finns, kör try, catch och kolla privilages på användaren genom token i form av roll
   try {
-    const verified = jwt.verify(token, 'secretKey');
+    const verified = jwt.verify(token, process.env.JWTSecret);
 
-    //Se om user har admin role privilages eller inte
-    if (verified.role === 'admin') {
-      res.json({ data: 'Secret data for admin!' });
-    } else {
+    if (verified == true) {
       res.json({ data: 'Secret data for user!' });
+    } else {
+      res.json({ message: "Not verified" });
     }
-
+    
   } catch {
     res.status(401).send('Invalid Token');
   }
